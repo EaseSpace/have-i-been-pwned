d9ab4a2c185d7dfab7f9a2e2c79e72ee648c577c
Thomas Heniart <heniart.thomas@gmail.com>
2018-04-17 13:12:48 +0200

    Add travis badge and fix some sonar lint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/EaseSpace/have-i-been-pwned.svg?branch=master)](https://travis-ci.org/EaseSpace/have-i-been-pwned)
 #Have I Been Pwned
 Java wrapper for have i been pwned API
--- src/main/java/haveibeenpwned/util/HashUtil.java
+++ src/main/java/haveibeenpwned/util/HashUtil.java
@@ -4,12 +4,17 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashUtil {
+
+    private HashUtil() {
+
+    }
+
     public static String sha1(String input) throws NoSuchAlgorithmException {
         MessageDigest mDigest = MessageDigest.getInstance("SHA1");
         byte[] result = mDigest.digest(input.getBytes());
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < result.length; i++) {
-            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        StringBuilder sb = new StringBuilder();
+        for (byte aResult : result) {
+            sb.append(Integer.toString((aResult & 0xff) + 0x100, 16).substring(1));
         }
         return sb.toString();
     }
